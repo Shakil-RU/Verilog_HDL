357387f8c444d1433e1012780e3b9ce7d4aa7ea3
Shakil hossan <81898266+Shakil-RU@users.noreply.github.com>
2022-11-19 20:49:44 +0600

    Update mod-10 asynchronous counter.v

--- Counter/Asynchronous/mod-10 asynchronous counter.v
+++ Counter/Asynchronous/mod-10 asynchronous counter.v
@@ -1,33 +1,30 @@
+module jk_ff(q,qbar,j,k,clear,clk);
+input j,k,clear,clk;
+output q,qbar;
+wire a,b,c,d,e,f,cbar;
+not (cbar,clk);
+nand n1(a,qbar,j,clear,clk);
+nand n2(b,clk,k,q);
+nand n3(c,a,d);
+nand n4(d,c,b,clear);
+nand n5(e,c,cbar);
+nand n6(f,d,cbar);
+nand n7(q,e,qbar);
+nand n8(qbar,q,f,clear);
+endmodule
 
-module mod_10_counter(q, clk, pre);
-	input clk, pre;
-	output [3:0] q;
-	jk_flip_flop jk1(q[0],,1,1,!(q[1]&q[3]),clk,pre);
-	jk_flip_flop jk2(q[1],,1,1,!(q[1]&q[3]),q[0],pre);
-	jk_flip_flop jk3(q[2],,1,1,!(q[1]&q[3]),q[1],pre);
-	jk_flip_flop jk4(q[3],,1,1,!(q[1]&q[3]),q[2],pre);
+module mod_10_counter(q,clear,clk);
+input clear,clk;
+output [3:0]q;
+wire w;
+nand (w,q[1],q[3]);
+jk_ff f1(q[0],,1,1,(w&clear),clk);
+jk_ff f2(q[1],,1,1,(w&clear),q[0]);
+jk_ff f3(q[2],,1,1,(w&clear),q[1]);
+jk_ff f4(q[3],,1,1,(w&clear),q[2]);
 endmodule
 
 
-module jk_flip_flop(q, qbar, j, k, clear, clk, pre);
 
-    input j, k, clear, clk, pre;
-    output q, qbar;
-    
-    wire n1_out, n2_out, n3_out, n4_out, n5_out, n6_out, cbar;
-    
-    nand nand1(n1_out, j, qbar, clk, clear, pre);
-    nand nand2(n2_out, k, clk, q);
-    
-    nand nand3(n3_out, n1_out, n4_out, pre);
-    nand nand4(n4_out, n2_out, n3_out, clear);
-    
-    not n1(cbar, clk);
-    
-    nand nand5(n5_out, n3_out, cbar);
-    nand nand6(n6_out, n4_out, cbar);
-    
-    nand nand7(q, n5_out, qbar, pre);
-    nand nand8(qbar, n6_out, q, clear);
-    
-endmodule
+
+
