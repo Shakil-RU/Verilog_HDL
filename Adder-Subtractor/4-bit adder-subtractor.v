99ec1ee07b9165b759fd384e16970bc472ec0228
Shakil hossan <81898266+Shakil-RU@users.noreply.github.com>
2022-11-19 16:41:41 +0600

    Update 4-bit adder-subtractor.v

--- Adder-Subtractor/4-bit adder-subtractor.v
+++ Adder-Subtractor/4-bit adder-subtractor.v
@@ -1,16 +1,17 @@
-module adder_subtractor(s, cout, a, b, mode);
+module adder_subtractor_false(s, cout, a, b, mode);
 	
 	input [3:0]a, b;
 	input mode;
 	output [3:0] s;
 	output cout;
 	
-	wire c1, c2, c3;
+	wire c1, c2, c3,c4;
 	
 	full_adder f_a1(s[0], c1, a[0], (b[0]^mode), mode);
 	full_adder f_a2(s[1], c2, a[1], (b[1]^mode), c1);
 	full_adder f_a3(s[2], c3, a[2], (b[2]^mode), c2);
 	full_adder f_a4(s[3], cout, a[3], (b[3]^mode), c3);
+        and(cout, ~c4, mode);
 
 endmodule
 
