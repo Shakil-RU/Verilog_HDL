4199d566f4118c83fda420a6eaf11b2867298881
Shakil hossan <81898266+Shakil-RU@users.noreply.github.com>
2022-11-19 17:33:58 +0600

    Update 4 bit ripple counter.v

--- Counter/Asynchronous/4 bit ripple counter.v
+++ Counter/Asynchronous/4 bit ripple counter.v
@@ -1,29 +1,24 @@
-
-module ripple_counter(q, q_bar, clear, clk);
-  
-	input clk, clear;
-	output [3:0] q, q_bar;
-	jk_flip_flop jk1(q[0], q_bar[0], 1, 1, clear, clk);
-	jk_flip_flop jk2(q[1], q_bar[1], 1, 1, clear, q[0]);
-	jk_flip_flop jk3(q[2], q_bar[2], 1, 1, clear, q[1]);
-	jk_flip_flop jk4(q[3], q_bar[3], 1, 1, clear, q[2]);
-  
+module jk_ff(q,qbar,j,k,clear,clk);
+input j,k,clear,clk;
+output q,qbar;
+wire a,b,y,ybar,e,d;
+not(cbar, clk);
+nand n1(a,qbar,j,clear,clk);
+nand n2(b,q,k,clk);
+nand n3(y,a,ybar);
+nand n4(ybar,clear,b,y);
+nand n5(e,cbar,y);
+nand n6(d,cbar,ybar);
+nand n7(q,e,qbar);
+nand n8(qbar,q,d,clear);
 endmodule
 
 
-module jk_flip_flop(q, q_bar, j, k, clear, clk);
-  
-	input j, k, clear, clk;
-	output q, q_bar;
-	wire a, b, c, d, e, f;
-	nand(a, q_bar, j, clk, clear);
-	nand(b, k, clk, q);
-	nand(c, a, d);
-	nand(d, c, clear, b);
-	not(clk_bar, clk);
-	nand(e, c, clk_bar);
-	nand(f, d, clk_bar);
-	nand(q, e, q_bar);
-	nand(q_bar, q, clear, f);
-  
+module ripple_counter(q,qbar,clear,clk);
+input clear,clk;
+output [3:0] q,qbar;
+jk_ff f1(q[0],qbar[0],1,1,clear,clk);
+jk_ff f2(q[1],qbar[1],1,1,clear,q[0]);
+jk_ff f3(q[2],qbar[2],1,1,clear,q[1]);
+jk_ff f4(q[3],qbar[3],1,1,clear,q[2]);
 endmodule
